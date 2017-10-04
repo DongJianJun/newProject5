b04d24240d35ae2fede64a28d55dcce297fc0d5b
Dan.Dong <Dan.Dong@riking.net>
2017-10-04 15:29:28 +0800

    update

--- MvcApplication6/Controllers/ValuesController.cs
+++ MvcApplication6/Controllers/ValuesController.cs
@@ -24,6 +24,7 @@ namespace MvcApplication6.Controllers
         // POST api/values
         public void Post([FromBody]string value)
         {
+            value = "";
         }
 
         // PUT api/values/5
